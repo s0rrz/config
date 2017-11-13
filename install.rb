425c2c56dfed2c176771d4457f8b4befd20a6d7e
s0rrz <srsorrz@gmail.com>
2017-11-13 11:25:00 +0100

    Installer, working on

--- install.rb
+++ install.rb
@@ -17,46 +17,38 @@ def packet_manager
 	end
 end
 
-def install_i3
-	if packet_manager == "apt"
-		system("apt-get install i3")
-	elsif packet_manager == "pacman"
-		system("pacman -S i3")
-	end
-end
-
-def install_zsh
-	if packet_manager == "apt"
-		system("apt-get install zsh")
-	elsif packet_manager == "pacman"
-		system("pacman -S zsh")
-	end
-	system('sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)" ')
-end
-
-
 
 def install_stuff
-	if !dir_exist('/usr/bin/i3')
-		install_i3
-
-	elsif !dir_exist('~/.i3')
-		exec('mkdir ~/.i3')
-	
-	elsif !dir_exist('/usr/bin/zsh')
-		install_zsh
+	if packet_manager == "apt"
+		system('apt-get install i3 zsh & sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)' )
+	elsif packet_manager == "pacman"
+		system('pacman -S i3 zsh & sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)' )
 	end
 
 
 end
 
 def copy_cfg
+	if !dir_exist('~/.i3')
+		exec('mkdir ~/.i3')
+	end
 	copy('i3/config', '~/.i3/config')
 	copy('vim/.vimrc', '~/.vimrc')
 	copy('zsh/.zshrc','~/.zshrc')
 end
 
+def start
+	puts "Press 1 to install zsh and i3, 2 to merge the config"
+	whato = gets.chomp
 
-install_stuff
-copy_cfg
+	if whato == "1"
+		install_stuff
+	elsif whato == "2"
+		copy_cfg
+	else
+		puts "Error"
+	end
 
+end
+
+start
